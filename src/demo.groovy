9b9e47b7f8ed36ab4c67b635e2c384fed9ff6106
mosinyang <mosinyang@xiaoe-tech.com>
2021-12-29 19:19:02 +0800

    test def

--- src/demo.groovy
+++ src/demo.groovy
@@ -1,6 +1,3 @@
-class demo {
-    static void main(String[] args) {
-        // Using a simple println statement to print output to the console
-        println('Hello World');
-    }
+def static void demo() {
+    println('Hello World');
 }
