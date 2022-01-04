956fef2502140b65be83c5a6dd2011c6d222519d
mosinyang <mosinyang@xiaoe-tech.com>
2022-01-04 19:27:11 +0800

    add class

--- src/devops/notify.groovy
+++ src/devops/notify.groovy
@@ -1,10 +1,11 @@
 package devops
 
-def notify(String webhookId, service, branch, commit, commitTime, commiter) {
+class Tool {
+    static def notify(String webhookId, service, branch, commit, commitTime, commiter) {
 
-    String content = " 项目:         ${service}\n 分支:         ${branch}\n 提交:         ${commit}\n 提交人:     @${commiter}\n 提交时间: ${commitTime}\n 部署成功"
+        String content = " 项目:         ${service}\n 分支:         ${branch}\n 提交:         ${commit}\n 提交人:     @${commiter}\n 提交时间: ${commitTime}\n 部署成功"
 
-    String message = """
+        String message = """
         {
             "msgtype": "text",
             "text": {
@@ -12,15 +13,19 @@ def notify(String webhookId, service, branch, commit, commitTime, commiter) {
             }
         }
     """
-    print(message);
+        print(message);
 
-    String url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${webhookId}";
-    URLConnection post = new URL(url).openConnection();
-    post.setRequestMethod("POST")
-    post.setDoOutput(true)
-    post.setRequestProperty("Content-Type", "application/json")
-    post.getOutputStream().write(message.getBytes("UTF-8"));
-    println(post.getInputStream().getText());
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${webhookId}";
+        URLConnection post = new URL(url).openConnection();
+        post.setRequestMethod("POST")
+        post.setDoOutput(true)
+        post.setRequestProperty("Content-Type", "application/json")
+        post.getOutputStream().write(message.getBytes("UTF-8"));
+        println(post.getInputStream().getText());
+    }
 }
+
+Tool t = new Tool()
+t.notify("3c676c3f-82a3-4e2a-9a49-df65b758e463", 1, 1, 1, 1,1)
 
 //notify("3c676c3f-82a3-4e2a-9a49-df65b758e463", 1, 1, 1, 1,1)
